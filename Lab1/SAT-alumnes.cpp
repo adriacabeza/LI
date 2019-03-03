213fb11757cbc74ea22c888ef476751e31d68c55
adriacabeza <adriacabeza@hotmail.com>
2019-03-03 19:18:19 +0100

    Lab 1 acabao

--- Lab1/SAT-alumnes.cpp
+++ Lab1/SAT-alumnes.cpp
@@ -8,7 +8,18 @@ using namespace std;
 #define TRUE 1
 #define FALSE 0
 
-const double MULT = 1e-5;     
+
+
+
+/*Tècnica VSIDS
+ * Heuristica: ordenes segons el nombre d'aparicions, sumes puntuació a les que han tingut conflicte
+ * i cada x iteracions ho divideixes per donar-li importància als conflictes més recents
+*/
+
+
+const double MULT = 0.75;     
+const int TIME_UPDATE = 1000;
+const int INCREMENT = 2;
 
 uint numVars;
 uint numClauses;
@@ -16,33 +27,31 @@ vector<vector<int> > clauses;
 vector<int> model;
 vector<int> modelStack;
 uint indexOfNextLitToPropagate;
-int conflictCnt;
-int propagationCnt;  
-int decisionCnt;
+uint conflictCnt;
+//int propagationCnt;  
+uint decisionCnt;
 uint decisionLevel;
 vector<vector<int> > taulaAux;
-vector<pair<int,int> > heuristic; //la heuristica tindrà la puntuació en el first i en el second el literal
+vector<int> heuristic;
     
 
-inline bool sortinrev(const pair<int,int> &a,  const pair<int,int> &b){
-  return (a.first > b.first); 
-} 
+inline void rescale() {
+  for (uint i=1; i<=numVars; ++i) heuristic[i] *= MULT;
+}
 
-// inline void rescale() {
-//   for (uint i=1; i<=numVars; ++i) heuristic[i].first *= MULT;
-// }
+inline void updateLiteralHeuristics(int index){
+  vector<int> literalClause = clauses[index];
+  int size = literalClause.size();
 
-inline void createHeuristica(vector<vector<int> >& taulaAux){
-  //fem que s'ordeni segons quants cops apareixen els literals a les clausules 
-  heuristic.resize(numVars);
-  for(uint i = 0; i < numVars; ++i){
-    heuristic[i] = make_pair(i,taulaAux[i].size());
+  for(int i = 0; i < size; ++i){
+    int lit = literalClause[i];
+    if(lit<0) heuristic[-lit] += INCREMENT;
+    else heuristic[lit] += INCREMENT; 
   }
-  std::sort(heuristic.begin(), heuristic.end(),sortinrev); //ordenat segons la seva puntuació 
 }
 
 
-void readClauses( ){
+inline void readClauses( ){
     
   // Skip comments
   char c = cin.get();
@@ -54,19 +63,23 @@ void readClauses( ){
   string aux;
   cin >> aux >> numVars >> numClauses;
   taulaAux.resize(numVars*2+1);
+  heuristic.resize(numVars+1,0.0);
   clauses.resize(numClauses);  
   // Read clauses
   for (uint i = 0; i < numClauses; ++i) {
     int lit;
     while (cin >> lit and lit != 0) {
         clauses[i].push_back(lit);
-        //em creo una taula amb posició totes les variables * 2(positius i negatius) i fico a totes les clausules a les que apareix
-        if(lit < 0) taulaAux[-lit+numVars].push_back(i);
-        else taulaAux[lit].push_back(i);
+        if(lit < 0) {
+          taulaAux[-lit+numVars].push_back(i);
+          heuristic[-lit] += INCREMENT;
+        }
+        else {
+          taulaAux[lit].push_back(i);
+          heuristic[lit] += INCREMENT;
+        }
     }
   }
-  createHeuristica(taulaAux);
-  
 }
 
 
@@ -95,23 +108,30 @@ bool propagateGivesConflict ( ) {
     
     ++indexOfNextLitToPropagate; 
     for (uint i = 0; i < taulaAux[lit].size(); ++i) {
-        
+        int literal_aux = taulaAux[lit][i]; 
       bool someLitTrue = false;
       int numUndefs = 0;
       int lastLitUndef = 0;
-      for (uint k = 0; not someLitTrue and k < clauses[taulaAux[lit][i]].size(); ++k){
-      
-        int val = currentValueInModel(clauses[taulaAux[lit][i]][k]);
+     
+      for (uint k = 0; not someLitTrue and k < clauses[literal_aux].size(); ++k){
+        int val = currentValueInModel(clauses[literal_aux][k]);
         if (val == TRUE) someLitTrue = true;
-        else if (val == UNDEF){ ++numUndefs; lastLitUndef = clauses[taulaAux[lit][i]][k]; }
+        else if (val == UNDEF){ 
+          ++numUndefs; 
+          lastLitUndef = clauses[literal_aux][k]; 
         }
+      }
+
+
         if (not someLitTrue and numUndefs == 0) {
              ++conflictCnt;
+             if(conflictCnt%TIME_UPDATE == 0) rescale();
+             updateLiteralHeuristics(literal_aux);
             return 1; // conflict! all lits false
         }
         else if (not someLitTrue and numUndefs == 1) setLiteralToTrue(lastLitUndef);	
     }    
-     ++propagationCnt;
+    // ++propagationCnt;
     }
   return 0;
 }
@@ -136,21 +156,21 @@ inline void backtrack(){
 
 
 // Heuristic for finding the next decision literal:
-// inline int getNextDecisionLiteral(){
-//   for (uint i = 1; i <= heuristic.size(); ++i) // stupid heuristic:
-//     if (model[heuristic[i].second] == UNDEF) return i;  // returns first UNDEF var, positively
-//   return 0; // returns 0 when all literals are defined
-// }
-
-// ORIGINAL 
-//Heuristic for finding the next decision literal:
-int getNextDecisionLiteral(){
-  for (uint i = 1; i <= numVars; ++i) // stupid heuristic:
-    if (model[i] == UNDEF) return i;  // returns first UNDEF var, positively
-  return 0; // reurns 0 when all literals are defined
-}
-
-
+  inline int getNextDecisionLiteral(){
+    int max_score = 0;
+    int index_max = 0;
+    for (uint i = 1; i <= numVars; ++i) {
+      if (model[i] == UNDEF) {
+          if(heuristic[i] > max_score){
+            max_score = heuristic[i];
+            index_max = i;
+          }
+          
+      }
+    }
+    ++decisionCnt;
+   return index_max; // returns 0 when all literals are defined
+ }
 
 void checkmodel(){
   for (uint i = 0; i < numClauses; ++i){
@@ -166,9 +186,9 @@ void checkmodel(){
   }  
 }
 inline void print_info(){
-  cout << "Decisions:    " << decisionCnt << endl;
-  cout << "Propagations: " << propagationCnt << endl;
-  cout << "Conflicts:    " << conflictCnt << endl;
+  cout << decisionCnt << " decisions"<< endl;
+  // cout << "Propagations: " << propagationCnt << endl;
+  // cout << "Conflicts:    " << conflictCnt << endl;
 }
 
 int main(){ 
@@ -177,7 +197,7 @@ int main(){
   indexOfNextLitToPropagate = 0;  
   decisionLevel = 0;
   conflictCnt = 0; 
-  propagationCnt = 0;
+  //propagationCnt = 0;
   decisionCnt = 0;
   
   // Take care of initial unit clauses, if any
@@ -195,32 +215,12 @@ int main(){
       if ( decisionLevel == 0) { cout << "UNSATISFIABLE" << endl; print_info(); return 10; }
       backtrack();
     }
-    // rescale();
     int decisionLit = getNextDecisionLiteral();
     if (decisionLit == 0) { checkmodel(); cout << "SATISFIABLE" << endl; print_info();  return 20; }
     // start new decision level:
     modelStack.push_back(0);  // push mark indicating new DL
     ++indexOfNextLitToPropagate;
     ++decisionLevel; 
-    ++decisionCnt;
     setLiteralToTrue(decisionLit);    // now push decisionLit on top of the mark
   }
 }
-
-
-
-// COMENTARI PROFE vector<int>& clausestovisit = lit<0?posList[-lit]:negList[lit];
-
-
-/*Tècnica VSIDS
- * Heuristica: ordenes segons el nombre d'aparicions, sumes puntuació a les que han tingut conflicte
- * i cada x iteracions ho divideixes per donar-li importància als conflictes més recents
-*/
-
-
-
-//un cop ja no tinc cap conflicte doncs he d'agafar del model stack quines son indefinides fent servir la heurística
-
-//propagació feta lo que has fet és fer una estructura de dades on tens els literals i les clausules on apareixen. Llavors accedeixes al seu negatiu per a que es generin conflictes i propagar.
-
-//Et falta fer la heurística d'ordenar el literal que es posarà a true i llavors fer la dinàmica (lo de anar fer swaps per el ordre), rebaixar la puntuació cada x instruccions
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# LI
+# Logic in Computer Science 
+
+### [Lab 1: Creating a SAT solver](https://github.com/adriacabeza/LI/tree/master/Lab1)
+
+How to execute:
+
+```bash
+sudo apt-get install picosat
+make
+./scriptPicoSAT.sh
+```
+
