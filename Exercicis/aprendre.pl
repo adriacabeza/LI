d33a880d237185698b4cc0e9969b53fdd024d7e1
lordsantanna <adriacabeza@hotmail.com>
2019-03-26 09:29:16 +0100

    exercicis fets

--- Exercicis/aprendre.pl
+++ Exercicis/aprendre.pl
@@ -63,7 +63,14 @@ prod([L1|L],P):- prod(L,P2), P = P2* L1.
 pescalar([],[],0).
 pescalar([X|L1],[Y|L2],R):- pescalar(L1,L2,N), R is X*Y + N. 
 
-%4. interseccion y unionFALTA INTERSECCIÓN
+%4. interseccion y union
+interseccion([],_,[]).
+interseccion([X|L1],[X|L2],[X|L]):- interseccion(L1,L2,L),!.
+interseccion([_|L1],L2,L3):-interseccion(L1,L2,L3).
+
+
+removeElement(X,L,NL):- concat(L1,[X|L2],L), concat(L1,L2,NL).
+removeElement2(X,L,NL):- append(L1,[X|L2],L), append(L1,L2,NL).
 
 union([],L,L).
 union([X|L1],L2,L3):- member(X,L2), !, union(L1,L2,L3).
@@ -142,3 +149,185 @@ nmembers(A,N,[X|L]):- nmembers(A,N1,L), pert(X,A), N1 is N-1.
 
 escribir([]):-write(' '),nl,!.
 escribir(X|L):- write(X), escribir(L).
+
+palindromos2(L1):- permutacion(L1,L), reverse(L,R), L = R,  write(R), nl, fail.
+
+
+palindromos(L) :- permutacion(L,P), es_palindromo(P), 
+  write(P), nl, fail. 
+palindromos(_). 
+
+es_palindromo([]).
+es_palindromo([_]) :- !. % regla adecuada
+es_palindromo([X|L]) :- concat(L1,[X],L), es_palindromo(L1). 
+
+palindroms(L) :- setof(P,(permutation(L,P), es_palindromo(P)),S), write(S). 
+
+
+%19
+suma([],[],[],C,C).
+suma([X1|L1],[X2|L2],[X3|L3],Cin,Cout) :-
+	X3 is (X1 + X2 + Cin) mod 10,
+	C  is (X1 + X2 + Cin) //  10,
+	suma(L1,L2,L3,C,Cout).
+
+
+send_more_money1 :-
+
+	L = [S, E, N, D, M, O, R, Y, _, _],
+	permutacion(L, [0,1,2,3,4,5,6,7,8,9]),
+	suma([D, N, E, S], [E, R, O, M], [Y, E, N, O], 0, M),
+
+	write('S = '), write(S), nl,
+	write('E = '), write(E), nl,
+	write('N = '), write(N), nl,
+	write('D = '), write(D), nl,
+	write('M = '), write(M), nl,
+	write('O = '), write(O), nl,
+	write('R = '), write(R), nl,
+	write('Y = '), write(Y), nl,
+	write('  '), write([S,E,N,D]), nl,
+	write('  '), write([M,O,R,E]), nl,
+	write('-------------------'), nl,
+	write([M,O,N,E,Y]), nl.
+
+
+inverso([],[]).
+inverso(L,[X|L1]):- concat(L2,[X],L), inverso(L2,L1).
+
+
+%21
+mis:- camino( [lado1,3,3], [lado2,0,0], [[lado1,3,3]] ).
+
+camino(Fin,Fin,Cam):- inverso(Cam,Sol), write(Sol), nl.
+camino(Ini,Fin,Cam):- paso(Ini,E), novisitado(E,Cam), camino(E,Fin,[E|Cam]).
+
+novisitado(E,Cam):- pert(E,Cam), !,fail.
+novisitado(_,_).
+
+paso( [lado1,M1,C1], [lado2,M2,C2] ):- pasan(M,C), M2 is M1-M, C2 is C1-C, safe(M2,C2).
+paso( [lado2,M1,C1], [lado1,M2,C2] ):- pasan(M,C), M2 is M1+M, C2 is C1+C, safe(M2,C2).
+
+pasan(M,C):- member( [M,C], [ [0,1], [0,2], [1,0], [1,1], [2,0] ] ).
+FALTA INTERSECCIÓN
+safe(M,C):- M>=0, M=<3, C>=0, C=<3, nocomen( M, C),
+            M1 is 3-M,  C1 is 3-C,  nocomen(M1,C1).
+
+nocomen(0,_).
+nocomen(M,C):- M>=C.
+
+%ANNA MESTRES EXERCICIS
+
+flatten([], []):- !.
+flatten(X, [X]):- X\=[_|_].
+flatten([L|List], FList):- flatten(L, L2), flatten(List, List2), append(L2, List2, FList).
+
+flattenNoRepetitions(L, RL):- flatten(L, L2), noRepes(L2, RL).
+
+noRepes([], []):- !.
+noRepes([L|List], [L|Res]):- \+member(L, List), noRepes(List, Res).
+noRepes([_|List], Res):- noRepes(List, Res).
+
+/*
+2. Tenemos una fila de cinco casas, con cinco vecinos con casas de colores diferentes, y cinco 
+profesiones, animales, bebidas y nacionalidades diferentes, y sabiendo que:
+    1 - El que vive en la casa roja es de Peru
+    2 - Al frances le gusta el perro
+    3 - El pintor es japones
+    4 - Al chino le gusta el ron
+    5 - El hungaro vive en la primera casa
+    6 - Al de la casa verde le gusta el coñac
+    7 - La casa verde esta a la izquierda de la blanca
+    8 - El escultor cría caracoles
+    9 - El de la casa amarilla es actor
+   10 - El de la tercera casa bebe cava
+   11 - El que vive al lado del actor tiene un caballo
+   12 - El hungaro vive al lado de la casa azul
+   13 - Al notario la gusta el whisky
+   14 - El que vive al lado del medico tiene un ardilla,
+Escribe un programa Prolog que averigue para cada persona todas sus 
+caracteristicas de la forma [num_casa,color,profesion,animal,bebida,pais] 
+averiguables. Ayuda: sigue el siguiente esquema: */
+
+vecinoIzq(R1, R2):- R2 is R1 + 1.
+vecinoDer(R1, R2):- R2 is R1 - 1.
+
+vecino(R1, R2):- vecinoIzq(R1, R2).
+vecino(R1, R2):- vecinoDer(R1, R2).
+
+casas:- Sol = [[1, _, _, _, _, _],
+               [2, _, _, _, _, _],
+               [3, _, _, _, _, _],
+               [4, _, _, _, _, _],
+               [5, _, _, _, _, _]],
+               member([_, roja, _, _, _, peru], Sol),
+               member([_, _, _, perro, _, francia], Sol),
+               member([_, _, pintor, _, _, japon], Sol),
+               member([_, _, _, _, ron, china], Sol),
+               member([1, _, _, _, _, hongria], Sol),
+               member([_, verde, _, _, coñac, _], Sol),
+               member([R1, verde, _, _, _, _], Sol),
+               member([R2, blanca, _, _, _, _], Sol),
+               vecinoIzq(R1, R2),
+               member([_, _, escultor, caracoles, _, _], Sol),
+               member([_, amarilla, actor, _, _, _], Sol),
+               member([3, _, _, _, cava, _], Sol),
+               member([R4, _, _, caballo, _, _], Sol),
+               member([R5, _, actor, _, _, _], Sol),
+               vecino(R4, R5),
+               member([R6, _, _, _, _, hongria], Sol),
+               member([R7, azul, _, _, _, _], Sol),
+               vecino(R6, R7),
+               member([_, _, notario, _, whisky, _], Sol),
+               member([R8, _, _, ardilla, _, _], Sol),
+               member([R9, _, medico, _, _, _], Sol),
+               vecino(R8, R9),
+               write(Sol), nl.
+
+
+
+/* AQUEST ME L'HE DE MIRAR
+3. Haz un programa prolog que escriba la manera de colocar sobre un tablero de
+   ajedrez ocho reinas sin que éstas se ataquen entre sí.
+   Por ejemplo, ésta sería una solucion:
+         
+      . . x . . . . .
+      . . . . . x . .
+      . . . x . . . .
+      . x . . . . . .
+      . . . . . . . x
+      . . . . x . . .
+      . . . . . . x . 
+      x . . . . . . . */
+      
+      
+
+
+      
+queens :- permutation([1,2,3,4,5,6,7,8], PermutedList),
+          allOK(PermutedList),
+          printQueens(PermutedList).
+
+% Se segur que no hi haura cap reina a la mateixa columna ni fila, 
+% nomes he de comprovar les diagonals
+allOK([]).
+allOK([L|List]):- checkQueen(L, List, 1), allOK(List).
+
+checkQueen(_, [], _).
+checkQueen(L, [L2|List], Dist):- L - L2 =\= Dist,
+                                 L2 - L =\= Dist,
+                                 Dist2 is Dist + 1,
+                                 checkQueen(L, List, Dist2).
+          
+% Printing code          
+printQueens([]).
+printQueens([L|List]):- printRow(L), nl, printQueens(List).
+
+printRow(L):- printPart(L), write('X'), printPart2(L).
+
+printPart(1):- !.
+printPart(L):- write('.'), L2 is L - 1, printPart(L2).
+
+printPart2(8):- !.
+printPart2(L):- write('.'), L2 is L + 1, printPart2(L2).
+
