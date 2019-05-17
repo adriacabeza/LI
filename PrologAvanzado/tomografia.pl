cb47603074114924bba9960f31bdc338086b3e6a
lord_santanna <adriacabezasantanna@gmail.com>
2019-05-17 15:56:43 +0200

    Add files via upload

---
+++ PrologAvanzado/tomografia.pl
@@ -0,0 +1,54 @@
+% A matrix which contains zeroes and ones gets "x-rayed" vertically and
+% horizontally, giving the total number of ones in each row and column.
+% The problem is to reconstruct the contents of the matrix from this
+% information. Sample run:
+%
+%	?- p.
+%	    0 0 7 1 6 3 4 5 2 7 0 0
+%	 0                         
+%	 0                         
+%	 8      * * * * * * * *    
+%	 2      *             *    
+%	 6      *   * * * *   *    
+%	 4      *   *     *   *    
+%	 5      *   *   * *   *    
+%	 3      *   *         *    
+%	 7      *   * * * * * *    
+%	 0                         
+%	 0                         
+%	
+
+:- use_module(library(clpfd)).
+
+ejemplo1( [0,0,8,2,6,4,5,3,7,0,0], [0,0,7,1,6,3,4,5,2,7,0,0] ).
+ejemplo2( [10,4,8,5,6], [5,3,4,0,5,0,5,2,2,0,1,5,1] ).
+ejemplo3( [11,5,4], [3,2,3,1,1,1,1,2,3,2,1] ).
+
+
+%podria comprovar que tenen la mateixa length
+checkSumMax([],[]).
+checkSumMax([X|R],[Y|Sum]):- sum(X, #=,Y), checkSumMax(R,Sum).
+
+p:-	ejemplo1(RowSums,ColSums),
+	length(RowSums,NumRows),
+	length(ColSums,NumCols),
+	NVars is NumRows*NumCols,
+	length(L,NVars),  % generate a list of Prolog vars (their names do not matter)
+	L ins 0..1, % Laca diu que ins es el domini
+	matrixByRows(L,NumCols,MatrixByRows),
+    transpose(MatrixByRows, MatrixByCols),
+	checkSumMax(MatrixByCols,ColSums),
+	checkSumMax(MatrixByRows,RowSums),
+	label(L),
+	pretty_print(RowSums,ColSums,MatrixByRows).
+
+	
+matrixByRows([],_,[]).
+matrixByRows(L,NumCols,[A|X]):- append(A,B,L), length(A,NumCols), matrixByRows(B,NumCols,X). 
+
+pretty_print(_,ColSums,_):- write('     '), member(S,ColSums), writef('%2r ',[S]), fail.
+pretty_print(RowSums,_,M):- nl,nth1(N,M,Row), nth1(N,RowSums,S), nl, writef('%3r   ',[S]), member(B,Row), wbit(B), fail.
+pretty_print(_,_,_).
+wbit(1):- write('*  '),!.
+wbit(0):- write('   '),!.
+    
